a703d04a18e91cbc869b2fa6c851957049c0bed4
Knyazb <Knyazb.serg@mail.ru>
2024-03-29 00:54:27 +0300

    init, add class LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        url_current = self.browser.current_url
+        assert 'login' in url_current, 'incorrect URL'
+
+    def should_be_login_form(self):
+
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM) , 'login_form not found'
+
+    def should_be_register_form(self):
+
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), 'register_form not found'
