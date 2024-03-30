09e0c15100ffb2ef790b7aa6ec9c1c344fe858fb
Knyazb <Knyazb.serg@mail.ru>
2024-03-30 14:06:01 +0300

    add func clik_add_to_basket

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,12 +5,15 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def clik_add_to_basket(self):
+        button_basket = self.browser.find_element(*MainPageLocators.BUTTON_BASKET)
+        button_basket.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
 
 
