bd35eb57e03c476f40575ccb3a97247b29629eb1
Knyazb <Knyazb.serg@mail.ru>
2024-03-30 14:05:02 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,5 +2,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BUTTON_BASKET = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    NAME_PRODUCT_ON_PAGE = (By.CSS_SELECTOR, ".product_main > h1")
+    MESSAGE_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages > div > div > strong')
+    PRICE_ON_PAGE = (By.CSS_SELECTOR, ".product_main > p.price_color")
+    MESSAGE_PRICE_ON_BASKET = (By.CSS_SELECTOR, '#messages > div.alert-info > div.alertinner > p > strong')
+
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
