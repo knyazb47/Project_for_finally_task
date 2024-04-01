33f1713c98f44c02e33062e6bd112a8b19ba7ef7
Knyazb <Knyazb.serg@mail.ru>
2024-04-01 20:56:54 +0300

    transferred methods click_button_view_basket and should_see_login_link v

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,19 @@
 
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import MainPageLocators, BasePageLocators
 
 class MainPage(BasePage):
 
     def clik_add_to_basket(self):
         button_basket = self.browser.find_element(*MainPageLocators.BUTTON_BASKET)
         button_basket.click()
+
+    def click_button_view_basket(self):
+        button_view_basket = self.browser.find_element(*MainPageLocators.BUTTON_VIEW_BASET)
+        button_view_basket.click()
+
+    def should_see_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), 'link is visible on the page'
 
 
 
