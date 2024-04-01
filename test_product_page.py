a597c46d4bd21a1cd947f881a025c3bfd15430e3
Knyazb <Knyazb.serg@mail.ru>
2024-04-01 21:10:44 +0300

    add for test mark need_review

--- test_product_page.py
+++ test_product_page.py
@@ -29,6 +29,7 @@ class TestUserAddToBasketFromProductPage():
         registration.register_new_user(email,password)
         registration.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = MainPage(browser, link)
         page.open()
@@ -67,13 +68,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)
@@ -82,7 +83,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page_basket = BasketPage(browser, browser.current_url)
     page_basket.item_is_not_basket()
     page_basket.message_basket_empty()
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     page = MainPage(browser, link)
     page.open()
