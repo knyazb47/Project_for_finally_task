33c12b6bd8ca4cdfdc35560a9c34f27b7cd94f89
Knyazb <Knyazb.serg@mail.ru>
2024-04-01 20:52:55 +0300

    add new LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     BUTTON_BASKET = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    BUTTON_VIEW_BASET = (By.CSS_SELECTOR, ".btn-group > a.btn-default")
     NAME_PRODUCT_ON_PAGE = (By.CSS_SELECTOR, ".product_main > h1")
     MESSAGE_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages > div > div > strong')
     PRICE_ON_PAGE = (By.CSS_SELECTOR, ".product_main > p.price_color")
@@ -10,7 +11,16 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
+    FILD_REGISTER_EMAIL = (By.ID, 'id_registration-email')
+    FILD_REGISTER_PASSWORD = (By.ID, 'id_registration-password1')
+    FILD_REGISTER_PASSWORD_CONFIRM = (By.ID, 'id_registration-password2')
+    BUTTON_REGISTER = (By.NAME, 'registration_submit')
 
 class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.ID, "login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BaskePageLocators():
+    ITEM_IN_BASKET = (By.CSS_SELECTOR, ".basket-items > div.row > div.col-sm-4 > h3 > a")
+    MESSAGE_BASKET_EMPTY = (By.CSS_SELECTOR, "#content_inner > p")
